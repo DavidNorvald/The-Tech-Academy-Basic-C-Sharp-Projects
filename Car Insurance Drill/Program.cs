b046e8d96bfda5088c5f69a820058c1b580fc01b
David N <davidnorvald@gmail.com>
2019-07-12 17:20:47 -0700

    Initial Commit
    Initial Commit for Car Insurance Drill

---
+++ Car Insurance Drill/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Car_Insurance_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you qualify for Auto Insurance?");
+            Console.WriteLine("Please answer the following questions:");
+            Console.WriteLine("What is your age?");
+            string ageNow = Console.ReadLine();
+            int currentAge = Convert.ToInt32(ageNow);
+
+            Console.WriteLine("Have you ever had a DUI? true or false");
+            string duiStatus = Console.ReadLine();
+            string yesDui = "true";
+            string noDui = "false";
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string ticketNum = Console.ReadLine();
+            int numOfTickets = Convert.ToInt32(ticketNum);
+
+            bool isQualified = (currentAge > 15 && duiStatus == "false" && numOfTickets <= 3);
+            Console.WriteLine("Qualified?: " + isQualified);
+            Console.ReadLine();
+             
+        }
+    }
+}
+
+
