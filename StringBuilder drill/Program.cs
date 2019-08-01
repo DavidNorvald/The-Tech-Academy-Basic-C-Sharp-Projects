830a804bc5cd464ac9aed756c8ef598c5e661142
David N <davidnorvald@gmail.com>
2019-08-01 11:13:41 -0700

    Initial commit for StringBuilder drill
    Commit for String and StringBuilder drill

---
+++ StringBuilder drill/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilder_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Dave";
+            string job = "Software Developer";
+            string school = "The Tech Academy";
+            string description = name + " is studying to be a " + job + " at " + school.ToUpper() + "!";
+
+            StringBuilder sb = new StringBuilder(50);
+            sb.Append("Hello World! ");
+            sb.AppendLine("This is my first attempt at using StringBuilder.");
+            sb.AppendLine("StringBuilder is making it easy to add new strings into my paragraph.");
+            sb.AppendLine("This is another line in my paragraph using AppendLine to create a new line.");
+
+            Console.WriteLine(description);
+            Console.ReadLine();
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
