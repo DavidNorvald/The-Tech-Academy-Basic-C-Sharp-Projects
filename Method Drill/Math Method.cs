d0ec2edcd15efe086d0ab791f5508a4f59cacf9c
David N <davidnorvald@gmail.com>
2019-08-08 22:57:15 -0700

    Initial commit for Method Drill
    Commit for Method drill

---
+++ Method Drill/Math Method.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Drill
+{
+    public class Math_Method
+    {
+        public static void Sum(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine($"Addition result is: {result}");     // $ represents text as UTF-16 chars when displayed to console
+        }
+
+        public static void Subtract(int num1, int num2)     //
+        {
+            int result = num1 - num2;
+            Console.WriteLine($"Substraction result is: {result}");     // $ represents text as UTF-16 chars when displayed to console
+        }
+
+        public static void Multiplication(int num1, int num2)
+        {
+            int result = num1 * num2;
+            Console.WriteLine($"Multiplication result is: {result}");       // $ represents text as UTF-16 chars when displayed to console
+        }
+
+
+
+    }
+}
---
+++ Method Drill/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets do some Math.");
+            Console.WriteLine("What number would you like to do some math operations on?");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number: ");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+
+            Math_Method.Sum(firstNum, secondNum);
+            Math_Method.Subtract(firstNum, secondNum);
+            Math_Method.Multiplication(firstNum, secondNum);
+
+            //Math_Method.Sum(firstNum, 4);
+            //Math_Method.Subtract(firstNum, 4);
+            //Math_Method.Multiplication(firstNum, 4);
+
+
+        }
+    }
+}
+
