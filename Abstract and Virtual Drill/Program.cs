f73ba6976e9122162c0af73f85f3011063669270
David N <davidnorvald@gmail.com>
2019-08-13 01:30:51 -0700

    Initial commit for Abstract and Virtual Drill
    commit for Abstract and Virtual Drill

---
+++ Abstract and Virtual Drill/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_and_Virtual_Drill
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            string Name = FirstName + " " + LastName;
+            Console.WriteLine(Name);
+        }
+    }
+
+    public class Employee : Person
+    {
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName();
+            Console.ReadLine();
+
+        }
+    }
+}
