7fe77bd60a006b399440b5df0b194e7a90d3b390
David N <davidnorvald@gmail.com>
2019-08-13 14:12:03 -0700

    Initial commit for Interface Drill
    commit for Interface Drill

---
+++ Interfaces Drill/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Interfaces_Drill
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            string Name = FirstName + " " + LastName;
+            Console.WriteLine(Name);
+        }
+    }
+
+    interface IQuittable
+    {
+        void Quit(Employee employee);
+    }
+
+    public class Employee : Person, IQuittable
+    {
+        public void Quit(Employee employee)
+        {
+            Console.WriteLine("I Quit.");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName();
+            employee.Quit(employee);
+            Console.ReadLine();
+
+        }
+    }
+}
