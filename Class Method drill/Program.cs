1d819676b6d3f7c0121e9d42e06ad98a5737fa86
David N <davidnorvald@gmail.com>
2019-08-12 23:45:00 -0700

    Initial commit for Class Method Drill
    commit for class method drill

---
+++ Class Method drill/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Class_Method_drill
+{
+    class divideNum
+    {
+        public void Divide(int num1)
+        {
+            int result = num1 / 2;
+            Console.WriteLine($"Division result is: {result}");     // $ represents text as UTF-16 chars when displayed to console
+        }
+
+        public static void Divide()
+        {
+            int result = 2 / 1;
+            Console.WriteLine($"Division result is: {result}");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            divideNum divide = new divideNum();
+
+            Console.WriteLine("Please enter a number");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            divide.Divide(userNum);
+            Console.ReadLine();
+        }
+    }
+}
