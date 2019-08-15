5e5d2c7b7c5299d1a5c210a8d94841f3d76defc6
David N <davidnorvald@gmail.com>
2019-08-15 15:59:36 -0700

    Initial commit for Enum Drill
    commit for Enum Drill

---
+++ Enum Drill/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Drill
+{
+    class Program
+    {
+
+        public enum DaysOfTheWeek
+        {
+            Wrong,
+            Monday = 1,
+            Tuesday = 2,
+            wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sunday = 7
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What day of the week is it today?");
+            string value = Console.ReadLine();
+            DaysOfTheWeek day;
+            try
+            {
+               day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), value);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week. Make sure it is capitalized if possible");
+                Console.WriteLine(ex.Message);
+                day = DaysOfTheWeek.Wrong;
+                Console.ReadLine();
+            }
+            if (day == DaysOfTheWeek.Thursday)
+            {
+                Console.WriteLine("Correct!");
+            }
+            else
+            {
+                Console.WriteLine("Not the correct day of the week.");
+            }
+
+
+
+            //DaysOfTheWeek currentDay = DaysOfTheWeek.Monday;       // enum's limit the possible values you can receive from a user     // helps limit errors from users
+
+            Console.ReadLine();
+        }
+    }
+}
