3150056556c0d642f521fe232fd41f5017b1817d
David N <davidnorvald@gmail.com>
2019-08-15 17:07:29 -0700

    Initial commit for Structs Drill
    commit for Structs Drill

---
+++ Structs Drill/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Structs_Drill
+{
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 3.14m;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
