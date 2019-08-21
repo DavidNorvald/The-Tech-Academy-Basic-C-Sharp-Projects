ee1f48f54df9622967a523dff4c0ec7d319fc4ba
David N <davidnorvald@gmail.com>
2019-08-21 15:40:05 -0700

    Initial commit for DateTime Drill
    commit for DateTime Drill

---
+++ DateTime Drill/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DateTime_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello! the current date and time is: " + DateTime.Now);
+            Console.WriteLine("Give me a number between 1 and 12.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("In {0} hour(s), it will be {1}", userNum, DateTime.Now.AddHours(userNum));
+
+            Console.ReadLine();
+        }
+    }
+}
