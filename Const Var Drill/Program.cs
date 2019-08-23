cf8cc25c649d6d41dc13b42a4005fd6c99cf49c6
David N <davidnorvald@gmail.com>
2019-08-22 21:38:04 -0700

    Initial commit for Const Var Drill
    commit for Const Var Drill

---
+++ Const Var Drill/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Const_Var_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string animalName = "Spot the dog";
+            var restOfThePhrase = "jumped over the barn";
+
+            Console.WriteLine("{0} {1}. Wow, that dog can jump!", animalName, restOfThePhrase);
+            Console.ReadLine();
+
+        }
+    }
+}
