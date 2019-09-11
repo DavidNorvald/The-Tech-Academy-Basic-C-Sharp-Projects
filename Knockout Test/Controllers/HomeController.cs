75c8ab2c5bf8703200fa4fe37d26169439ed9058
David N <davidnorvald@gmail.com>
2019-09-11 15:49:35 -0700

    Initial commit for Final C# project (Knockout)
    commit for final project with C#, chose Knockout.js

---
+++ Knockout Test/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Knockout_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Knockout()
+        {
+            ViewBag.Message = "Sample of using Knockout.js, HTML, and JQuery";
+
+            return View();
+        }
+
+
+    }
+}
